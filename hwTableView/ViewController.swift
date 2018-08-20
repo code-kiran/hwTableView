86066e30e0e6ab3c665ab9dde16813f6790fa355
kiran <kiran@kirans-pc.local>
2018-08-20 16:03:35 +0545

    Initial Commit

---
+++ hwTableView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hwTableView
+//
+//  Created by kiran on 8/20/18.
+//  Copyright © 2018 kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
